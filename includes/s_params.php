f24ed4bfdb55779a90ed50edd2c83b6324d78ea8
Mustafa Erdinç <digitallartist@gmail.com>
2023-11-24 15:07:47 +0300

    walllet update

--- includes/s_params.php
+++ includes/s_params.php
@@ -1,8 +1,8 @@
 <?php
 
 $secret_params=array();
-$secret_params['Kraken']['mustafa'][0]['key']='97vAAjU8yynpjyIe1lDsAkIVPzCrRPKiKB1DJPXI+zPPP4S4WCauDY7m';//api-key-1626092349335 wallet, order, withdraw 
-$secret_params['Kraken']['mustafa'][0]['secret']='BS8MyrddsHSta1uzhyCY30ggGvbg2sy0yok0DPErRtF9Ly9NSRJiOatmW5/a2KEV2/DSa8FqWDbzpXPPzlZufQ=='; 
+$secret_params['Kraken']['mustafa'][0]['key']='3vI/oo2FneQ3j3Ra+EAfD34fLkYfP1mj5AS4B1CGvE1zjoitYhYUIPOI';//api-key-1626092349335 wallet, order, withdraw 
+$secret_params['Kraken']['mustafa'][0]['secret']='d/CGjzT/kYDHYEO6Suu2B+kqqU4StgBVA/l8fzyOKwemImcxOhJ9qL36Jxo75VC8XVm8NOmuCcRAPPotaFXTbw=='; 
 $secret_params['Kraken']['deniz'][0]['key']='97vAAjU8yynpjyIe1lDsAkIVPzCrRPKiKB1DJPXI+zPPP4S4WCauDY7m';//api-key-1626092349335 wallet, order, withdraw 
 $secret_params['Kraken']['deniz'][0]['secret']='BS8MyrddsHSta1uzhyCY30ggGvbg2sy0yok0DPErRtF9Ly9NSRJiOatmW5/a2KEV2/DSa8FqWDbzpXPPzlZufQ=='; 
 
@@ -13,7 +13,7 @@ $secret_params['BtcTurk']['mustafa'][1]['key']='63f9bea0-dad2-46b1-bc47-1f99a46b
 $secret_params['BtcTurk']['mustafa'][1]['secret']='9yZ/yTMq5AJzp5B4Ndl9ZXsv4U3eR3nC';//açık emirler
 $secret_params['BtcTurk']['mustafa'][2]['key']='247bd944-8606-445f-966e-abfb39c431dd'; //sipraiş
 $secret_params['BtcTurk']['mustafa'][2]['secret']='BfWfxqzWxhTt+oOsquxmHwf3ETIOHS0O';//sipariş
-$secret_params['BtcTurk']['mustafa'][3]['key']='7966d4d5-953c-4c72-ac66-6e20bc2bf239'; //herşey
-$secret_params['BtcTurk']['mustafa'][3]['secret']='ICx7W/t+JNwkpERtoaafI9LBjjIlsUaf';//herşey
+$secret_params['BtcTurk']['mustafa'][3]['key']='9ed64d82-0325-4a1d-ac74-049e3665a162'; //herşey
+$secret_params['BtcTurk']['mustafa'][3]['secret']='ST2B+VcpFRodohJ4IQFSeyFIvBvCIf3K';//herşey
 
 ?>
